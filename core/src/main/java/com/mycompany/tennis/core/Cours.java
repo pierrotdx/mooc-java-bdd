679e01fc87dc301c9696f889fb91e019e4858090
pierrotdx <pierrotdx@gmail.com>
2025-08-18 22:36:34 +0200

    21. Un service métier plus complexe

---
+++ core/src/main/java/com/mycompany/tennis/core/Cours.java
@@ -0,0 +1,36 @@
+package com.mycompany.tennis.core;
+
+import com.mycompany.tennis.core.entity.*;
+import com.mycompany.tennis.core.service.JoueurService;
+import com.mycompany.tennis.core.service.MatchService;
+import com.mycompany.tennis.core.service.TournoiService;
+
+public class Cours {
+    public static void main(String... args){
+        MatchService matchService = new MatchService();
+
+        Match match = new Match();
+
+        Score score = new Score();
+        score.setSet1((byte)3);
+        score.setSet2((byte)4);
+        score.setSet3((byte)6);
+        match.setScore(score);
+        score.setMatch(match);
+
+        Joueur federer = new Joueur();
+        federer.setId(32l); // pas besoin de plus dans notre cas mais on pourrait récupérer toutes les infos avec JoueurService
+        match.setVainqueur(federer);
+
+        Joueur murray = new Joueur();
+        murray.setId(34l);
+        match.setFinaliste(murray);
+
+        Epreuve epreuve = new Epreuve();
+        epreuve.setId(10l);
+        match.setEpreuve(epreuve);
+
+        matchService.enregistrerNouveauMatch(match);
+        System.out.println("L'identifiant du match créé est " + match.getId().toString());
+    }
+}
---
+++ core/src/main/java/com/mycompany/tennis/core/repository/ScoreRepositoryImpl.java
@@ -0,0 +1,68 @@
+package com.mycompany.tennis.core.repository;
+
+import com.mycompany.tennis.core.entity.Score;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+import static com.mycompany.tennis.core.DataSourceProvider.getSingleDataSourceInstance;
+
+public class ScoreRepositoryImpl {
+    public void create(Score score) {
+        Connection conn = null;
+        try {
+            DataSource dataSource = getSingleDataSourceInstance();
+
+            conn = dataSource.getConnection();
+
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO SCORE_VAINQUEUR (ID_MATCH, SET_1, SET_2, SET_3, SET_4, SET_5) VALUES (?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            statement.setLong(1, score.getMatch().getId());
+            statement.setByte(2, score.getSet1());
+            statement.setByte(3, score.getSet2());
+            if (score.getSet3() == null) {
+                statement.setNull(4, Types.TINYINT);
+            } else {
+                statement.setByte(4, score.getSet3());
+            }
+            if (score.getSet4() == null) {
+                statement.setNull(5, Types.TINYINT);
+            } else {
+                statement.setByte(5, score.getSet4());
+            }
+            if (score.getSet5() == null) {
+                statement.setNull(6, Types.TINYINT);
+            } else {
+                statement.setByte(6, score.getSet5());
+            }
+
+
+            statement.executeUpdate();
+
+            ResultSet rs = statement.getGeneratedKeys();
+
+            if (rs.next()) {
+                score.setId(rs.getLong(1));
+            }
+
+            System.out.println("Score créé.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+        finally {
+            try {
+                if (conn!=null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
