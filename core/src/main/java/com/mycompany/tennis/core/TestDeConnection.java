54c3dd0444752111279ddefdf60f42adbc161514
pierrotdx <pierrotdx@gmail.com>
2025-08-18 10:04:30 +0200

    première requête : lecture

--- core/src/main/java/com/mycompany/tennis/core/TestDeConnection.java
+++ core/src/main/java/com/mycompany/tennis/core/TestDeConnection.java
@@ -1,14 +1,23 @@
 package com.mycompany.tennis.core;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TestDeConnection {
     public static void main(String... args){
         Connection conn = null;
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris&allowPublicKeyRetrieval=true","COURSDB","coursdb");
+
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT NOM, PRENOM, ID FROM JOUEUR");
+
+            while (rs.next()) {
+                String nom = rs.getString("NOM");
+                String prenom = rs.getString("PRENOM");
+                final long id = rs.getInt("ID");
+                System.out.printf("Le/la joueur/joueuse %s %s est représenté(e) par l'id %d%n.", prenom, nom, id);
+            }
+
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
