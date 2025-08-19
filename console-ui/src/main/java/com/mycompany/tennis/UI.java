783ae0559862f5f8400652197e7b049ff014299b
pierrotdx <pierrotdx@gmail.com>
2025-08-19 11:17:58 +0200

    28. Configuration d'Hibernate

--- console-ui/src/main/java/com/mycompany/tennis/UI.java
+++ console-ui/src/main/java/com/mycompany/tennis/UI.java
@@ -5,7 +5,7 @@ import com.mycompany.tennis.controller.TournoiController;
 
 public class UI {
     public static void main(String[] args) {
-        TournoiController tournoiController = new TournoiController();
+                TournoiController tournoiController = new TournoiController();
         tournoiController.creerTournoi();
     }
 }
